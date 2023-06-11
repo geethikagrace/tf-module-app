7ab3d605a09b0dc55a5356b32938907642e3ba02
geethika <geethilka2219@gmail.com>
2023-06-11 03:35:26 -0700

    web subnets

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-#resource "aws_instance" "test"{
-#  ami = "03265a0778a880afb"
-#  instance_type = var.instance_type
-#  subnet_id = var.subnet_id
-#}
-#
-#variable "instance_type" {}
-#variable "subnet_id" {}
+resource "aws_instance" "test"{
+  ami = "03265a0778a880afb"
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+}
+
+variable "instance_type" {}
+variable "subnet_id" {}
