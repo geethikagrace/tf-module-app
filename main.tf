afc5517db38964b3a42b0145ab34fd55be6c2172
geethika <geethilka2219@gmail.com>
2023-06-28 12:58:58 +0530

    elasticache

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns   = [ aws_lb_target_group.main.arn ]
 
   launch_template {
     id      = aws_launch_template.template.id
@@ -56,4 +57,12 @@ resource "aws_autoscaling_group" "asg" {
       value               = tag.value
     }
   }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  tags     = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
 }
