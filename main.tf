b089ec072f5aba2debbb9f2cb51d21285ca7aa2a
geethika <geethilka2219@gmail.com>
2023-07-04 00:14:22 +0530

    target group

--- main.tf
+++ main.tf
@@ -66,3 +66,17 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
   tags     = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
 }
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.listener_arn
+  priority     =var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    field  = "host-header"
+    values = [ local.dns_name]
+}
