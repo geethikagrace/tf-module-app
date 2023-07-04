ccbe9e39ed4f5de28bcb939f0ba1274e48c17f2d
geethika <geethilka2219@gmail.com>
2023-07-04 19:43:26 +0530

    editing autuscaling group

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,3 @@
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/geethikagrace/roboshop-ansible  roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
 
-#ansible-pull -i localhost, -U https://github.com/geethikagrace/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${name} &>>/opt/ansible.log
