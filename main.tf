e2428b271699998d8d0ae0ae77ef3a968c9a423e
geethika <geethilka2219@gmail.com>
2023-07-05 22:02:54 +0530

    troublshooting nginx

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_route53_record" "main" {
-  zone_id = var.domain_id
+  zone_id = var.zone_id
   name    = local.dns_name
   type    = "CNAME"
   ttl     = 30
